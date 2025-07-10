4c095a2e28e502114336bf13bb5853c8854bf2e1
ANavarrete00 <adrian.navarrete@yahoo.com>
2025-07-09 20:33:17 -0700

    implamenting cloudflare

--- src/components/TurnstileGate.jsx
+++ src/components/TurnstileGate.jsx
@@ -3,15 +3,11 @@ import { Turnstile } from '@marsidev/react-turnstile'
 export default function TurnstileGate() {
     const SITE_KEY = process.env.REACT_APP_TURNSTILE_SITE_KEY;
 
-    async function handle(event) {
+    async function handle(token) {
         try {
-            event.preventDefault();
-            const formData = new FormData();
-            const token = formData.get("cf-turnstile-response");
-
             const response = await fetch("/.netlify/functions/verify-turnstile", {
                 method: "POST",
-                body: JSON.stringify({token}),
+                body: JSON.stringify({ token }),
                 headers: {
                     "content-type": "application/json",
                 }
@@ -20,17 +16,16 @@ export default function TurnstileGate() {
             const data = await response.json()
             if (data.success) {
                 // on success code here
+                console.log("Server verified token successfully.");
             }
-            console.log("Manual cookie set: ", document.cookie);
         } catch (e) {
             console.error("Verification failed: ", e);
         }
     }
 
-
     return (
         <>
-            <Turnstile siteKey={ SITE_KEY } />
+            <Turnstile siteKey={ SITE_KEY } onSuccess={ handle } />
         </>
     )
 }
