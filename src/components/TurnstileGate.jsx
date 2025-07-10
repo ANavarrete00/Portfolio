c9e17079e149eabd2c749705504b916112bf991d
ANavarrete00 <adrian.navarrete@yahoo.com>
2025-07-09 20:16:44 -0700

    implamenting cloudflare

--- src/components/TurnstileGate.jsx
+++ src/components/TurnstileGate.jsx
@@ -1,10 +1,7 @@
-import { useEffect, useState, useRef } from "react";
 import { Turnstile } from '@marsidev/react-turnstile'
 
 export default function TurnstileGate() {
-    const [verified, setVerified] = useState(false);
     const SITE_KEY = process.env.REACT_APP_TURNSTILE_SITE_KEY;
-    const ref = useRef();
 
     async function handle(event) {
         try {
@@ -31,7 +28,6 @@ export default function TurnstileGate() {
                 if (script) {
                     script.remove();
                 }
-                setVerified(true);
             }
             console.log("Manual cookie set: ", document.cookie);
         } catch (e) {
@@ -39,14 +35,10 @@ export default function TurnstileGate() {
         }
     }
 
-    function handleSuccess(event) {
-        document.cookie = "verified=true; max-age=3600; path=/; SameSite=Lax";
-        setVerified(true);
-    }
 
     return (
         <div onLoad={handle}>
-            <Turnstile siteKey={ SITE_KEY } onSuccess={handleSuccess} />
+            <Turnstile siteKey={ SITE_KEY } />
         </div>
     )
 }
